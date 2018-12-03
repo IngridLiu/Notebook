702b49ff52e76348a660ba27c012a16504141070
Ingrid <895474756@qq.com>
2018-12-03 10:53:22 +0800

    tf入门

--- TensorFlow学习笔记/test.py
+++ TensorFlow学习笔记/test.py
@@ -7,6 +7,8 @@
 #
 # x = 1
 
+# ---------------------------------------------------------------
+
 import tensorflow as tf
 
 # 创建一个常量 op,产生一个1x2矩阵，这个被作为一个节点
@@ -37,3 +39,35 @@ print(result)
 
 # 任务完成，关闭会话
 sess.close()
+
+# ---------------------------------------------------------------
+
+# 创建一个变量，初始化为标量0
+state = tf.Variable(0, name = 'counter')
+
+# 创建一个op，其作用是使state增加1
+one = tf.constant(1)
+new_value = tf.add(state, one)
+update = tf.assign(state, new_value)
+
+# 启动图后，变量必须先经过'初始化'（init）op初始化
+# 首先必须增加一个'初始化'op到图中
+init_op = tf.initialize_all_variables()
+
+# 启动图，运行op
+with tf.Session() as sess:
+    # 运行 'init' op
+    sess.run(init_op)
+    # 打印 'state'的初始值
+    print(sess.run(state))
+    # 运行op，更新'state'，并打印'state'
+    for _ in range(3):
+        sess.run(update)
+        print(sess.run(state))
+
+# 输出:
+# 0
+# 1
+# 2
+# 3
+
